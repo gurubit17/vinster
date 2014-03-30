7d41bf49317fbaf68ff89f679724494640609a3a
Nota_Guru_inCode <gurubit17@gmail.com>
2014-03-30 15:30:38 +0000

    sun 1st

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,16 @@ class PostsController < ApplicationController
 		# WHEN SOMEONE GOES TO /posts
 	end
 
+	def new
+		@post = Post.new
+	end
+
+	def create
+		Post.create (post_params)
+		redirect_to posts_path
+	end	
+
+	def post_params
+		params.require(:post).permit(:title, :description)
+	end	
 end
